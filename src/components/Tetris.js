69110ce9967bd8e458a01ddfa45860de42b5af28
aindriu80 <aindriu80@gmail.com>
2020-10-05 21:17:16 +0100

    Player Rotation

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { createStage, checkCollision } from '../gameHelpers';
+import { createStage, checkCollision } from "../gameHelpers";
 
 // Styled Components
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 // Custom Hooks
-import { usePlayer } from '../hooks/usePlayer';
-import { useStage } from '../hooks/useStage';
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
-  console.log('re-render');
+  console.log("re-render");
 
   const movePlayer = (dir) => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
@@ -42,7 +42,7 @@ const Tetris = () => {
     } else {
       // Game Over
       if (player.pos.y < 1) {
-        console.log('Game Over!');
+        console.log("Game Over!");
         setGameOver(true);
         setDropTime(null);
       }
@@ -62,6 +62,8 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -3,7 +3,7 @@ export const STAGE_HEIGHT = 20;
 
 export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
-    new Array(STAGE_WIDTH).fill([0, 'clear']),
+    new Array(STAGE_WIDTH).fill([0, "clear"])
   );
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
@@ -19,7 +19,7 @@ export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
           // 4. Check that the cell we're moving to isn't set to clear
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
-            'clear'
+            "clear"
         ) {
           return true;
         }
